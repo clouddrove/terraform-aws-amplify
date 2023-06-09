9c0444c6328b8c29ba1e491665e23c66983ff11a
nileshgadgi <nilesh.gadgi@clouddrove.com>
2023-06-09 23:35:04 +0530

    enh: Restructure whole module with new values and structure

--- _example/example.tf
+++ _example/example.tf
@@ -5,37 +5,69 @@ provider "aws" {
 module "amplify" {
   source = "../"
 
-  name        = "amplify"
-  environment = "test"
+  name        = "test"
+  environment = "amplify"
   label_order = ["name", "environment"]
 
-  amplify_enabled        = true
-  environment_name       = "prod"
-  domain_name            = ["test.eu", "test.dk"]
-  amplify_repository     = "https://github.com/clouddrove/terraform-aws-amplify"
-  access_token           = "ghxxxxxm06Sd3KqkafBVu0xzcxcxcv0EA8AK"
-  amplify_branch_name    = "main"
+  amplify_enabled  = true
+  environment_name = "staging"
+  platform         = "WEB"
+
+  auto_branch_creation_config = {
+    # Enable auto build for the created branches
+    enable_auto_build = true
+  }
+  enable_auto_branch_creation = false
+  enable_basic_auth           = false
+  enable_branch_auto_build    = true
+  enable_branch_auto_deletion = true
+
+  auto_branch_creation_patterns = [
+    "*",
+    "*/**"
+  ]
+
+  domain_name            = ["test-example.com", "www.test-example.com"]
+  amplify_repository     = "https://github.com/nileshgadgi/gitrepo"
+  access_token           = "ghp_xxxxxjq0mUrbxxxxxxxuqxh4pxxxxxxx192m"
   sub_domain_prefix_name = "scan"
   deployment_artifacts   = "app-example-deployment"
-  stack_name             = "amplify-app-example"
-  branch_framework       = "React"
+  branch_framework       = "Web"
   branch_stage           = "PRODUCTION"
+  ttl                    = 5
+  build_spec             = <<-EOT
+    version: 1
+    frontend:
+      phases:
+        # IMPORTANT - Please verify your build commands
+        build:
+          commands: []
+      artifacts:
+        # IMPORTANT - Please verify your build output directory
+        baseDirectory: /
+        files:
+          - '**/*'
+      cache:
+        paths: []
+  EOT
 
-  # Environment variables for the autocreated branch.
-  branch_environment_variables = {
-    REACT_APP_API_SERVER = "https://api.example.com"
-
-  }
   # Environment variables for the branch.
   amplify_app_environment_variables = {
-    ENV = "test"
+    ENV = "master"
   }
 
   custom_rules = [
     {
-      source = "/01/<p01>/21/<p21>"
-      status = "302"
-      target = "/?p01=<p01>&p21=<p21> "
+      source = "/<*>"
+      status = "404"
+      target = "/index.html"
     }
   ]
+
+  branches = {
+    branch_name       = "master"
+    framework         = "WEB"
+    stage             = "DEVELOPMENT"
+    enable_auto_build = true
+  }
 }
--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+locals {
+  environments = { for k, v in var.branches : k => v if var.amplify_enabled }
+}
+
 module "labels" {
   source  = "clouddrove/labels/aws"
   version = "1.3.0"
@@ -11,28 +15,14 @@ module "labels" {
 resource "aws_amplify_app" "example" {
   count = var.amplify_enabled ? 1 : 0
 
-  name       = module.labels.id
-  repository = var.amplify_repository
+  name                          = module.labels.id
+  repository                    = var.amplify_repository
+  description                   = var.description
+  platform                      = var.platform
+  auto_branch_creation_patterns = var.auto_branch_creation_patterns
 
   # The default build_spec added by the Amplify Console for React.
-  build_spec = <<-EOT
-    version: 0.1
-    frontend:
-      phases:
-        preBuild:
-          commands:
-            - yarn install
-        build:
-          commands:
-            - yarn run build
-      artifacts:
-        baseDirectory: build
-        files:
-          - '**/*'
-      cache:
-        paths:
-          - node_modules/**/*
-  EOT
+  build_spec = var.build_spec
 
   # The default rewrites and redirects added by the Amplify Console.
   dynamic "custom_rule" {
@@ -46,17 +36,38 @@ resource "aws_amplify_app" "example" {
     }
   }
 
-  environment_variables = var.amplify_app_environment_variables
-  access_token          = var.access_token
-  tags                  = module.labels.tags
+  environment_variables       = var.amplify_app_environment_variables
+  access_token                = var.access_token
+  oauth_token                 = var.oauth_token
+  enable_auto_branch_creation = var.enable_auto_branch_creation
+  enable_branch_auto_deletion = var.enable_branch_auto_deletion
+  enable_basic_auth           = var.enable_basic_auth
+  enable_branch_auto_build    = var.enable_branch_auto_build
+
+  dynamic "auto_branch_creation_config" {
+    for_each = var.auto_branch_creation_config != null ? [true] : []
+
+    content {
+      basic_auth_credentials        = lookup(var.auto_branch_creation_config, "basic_auth_credentials", null)
+      build_spec                    = lookup(var.auto_branch_creation_config, "build_spec", null)
+      enable_auto_build             = lookup(var.auto_branch_creation_config, "enable_auto_build", null)
+      enable_basic_auth             = lookup(var.auto_branch_creation_config, "enable_basic_auth", null)
+      enable_performance_mode       = lookup(var.auto_branch_creation_config, "enable_performance_mode", null)
+      enable_pull_request_preview   = lookup(var.auto_branch_creation_config, "enable_pull_request_preview", null)
+      environment_variables         = lookup(var.auto_branch_creation_config, "environment_variables", null)
+      framework                     = lookup(var.auto_branch_creation_config, "framework", null)
+      pull_request_environment_name = lookup(var.auto_branch_creation_config, "pull_request_environment_name", null)
+      stage                         = lookup(var.auto_branch_creation_config, "stage", null)
+    }
+  }
+  tags = module.labels.tags
 }
 
 resource "aws_amplify_backend_environment" "example" {
-  count = var.amplify_enabled ? 1 : 0
+  count = var.amplify_enabled && var.backend_enable ? 1 : 0
 
-  app_id           = join("", aws_amplify_app.example.*.id)
-  environment_name = var.environment_name
-
+  app_id               = join("", aws_amplify_app.example.*.id)
+  environment_name     = var.environment_name
   deployment_artifacts = var.deployment_artifacts
   stack_name           = var.stack_name
 }
@@ -64,13 +75,15 @@ resource "aws_amplify_backend_environment" "example" {
 resource "aws_amplify_branch" "main" {
   count = var.amplify_enabled ? 1 : 0
 
-  app_id      = join("", aws_amplify_app.example.*.id)
-  branch_name = var.amplify_branch_name
-
-  framework = var.branch_framework
-  stage     = var.branch_stage
-
-  environment_variables = var.branch_environment_variables
+  app_id                = join("", aws_amplify_app.example.*.id)
+  branch_name           = var.branches.branch_name
+  display_name          = var.branches.display_name
+  description           = var.branches.description
+  framework             = var.branches.framework
+  stage                 = var.branches.stage
+  enable_auto_build     = var.branches.enable_auto_build
+  ttl                   = var.branches.ttl
+  environment_variables = var.branches.environment_variables
   tags                  = module.labels.tags
 }
 
