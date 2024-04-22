d587fe915244c864b5b3dd54d8720eb4219e91b4
Vishwajit Nagulkar <119565952+VishwajitNagulkar@users.noreply.github.com>
2024-04-22 18:17:56 +0530

    feat- updated version

--- examples/versions.tf
+++ examples/versions.tf
@@ -1,3 +1,4 @@
+
 # Terraform version
 terraform {
   required_version = ">= 1.6.6"
@@ -5,7 +6,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.45.0"
+      version = "~> 5.46.0"
     }
   }
 }
